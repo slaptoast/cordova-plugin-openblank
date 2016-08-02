ea8e82f4a715a3ca3ff5c8c49ecf65d8fa493748
Terry Johnson <terry.johnson@dowjones.com>
2016-08-02 13:47:05 -0500

    comment everything out

--- src/ios/CDVUIWebViewDelegate+OpenBlank.h
+++ src/ios/CDVUIWebViewDelegate+OpenBlank.h
@@ -1,12 +1,12 @@
 
-#import <UIKit/UIKit.h>
+// #import <UIKit/UIKit.h>
 
-@interface CDVUIWebViewDelegate+OpenBlank : NSObject <UIWebViewDelegate>{
-    __weak NSObject <UIWebViewDelegate>* _delegate;
-    NSInteger _loadCount;
-    NSInteger _state;
-    NSInteger _curLoadToken;
-    NSInteger _loadStartPollCount;
-}
+// @interface CDVUIWebViewDelegate : NSObject <UIWebViewDelegate>{
+//     __weak NSObject <UIWebViewDelegate>* _delegate;
+//     NSInteger _loadCount;
+//     NSInteger _state;
+//     NSInteger _curLoadToken;
+//     NSInteger _loadStartPollCount;
+// }
 
-@end
+// @end
