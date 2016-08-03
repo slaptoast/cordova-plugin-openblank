03b2abdc66a94956999ce56b707eea43c0e3903d
Terry Johnson <terry.johnson@dowjones.com>
2016-08-03 11:45:00 -0500

    adding logging

--- src/android/OpenBlank.java
+++ src/android/OpenBlank.java
@@ -1,9 +1,24 @@
+
+
 package org.apache.cordova.openblank;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaHttpAuthHandler;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.LOG;
+import org.apache.cordova.PluginManager;
+import org.apache.cordova.PluginResult;
+
+import android.util.Log;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class OpenBlank extends CordovaPlugin {
     @Override
 	   public Boolean shouldOpenExternalUrl(String url) {
+	   		Log.d("OpenBlank Plugin called with URL " + url);
 	   		if(url.indexOf("utm_content") > -1) {
 	   			return true;
 	   		}
